292fbea434bef53e171c03caa916a356bd734c22
hiryu826 <45277948+hiryu826@users.noreply.github.com>
2019-10-28 23:51:37 +0900

    Fist JS

---
+++ Index.js
@@ -0,0 +1,20 @@
+console.log('My name is Ryu Se Hyun, I can do it');
+
+const Num = 100 * 100;
+let a = 'Ryu';
+let b = a + 'Sehyun'; 
+a = 100;
+console.log(Num, b, a); 
+
+/* String(문자열 - Text/모든걸 같이 붙인다.)
+const what = "String"
+console.log(what);*/
+
+/* Boolean(True or False - 모두 소문자로 쓴다)
+const what = true; // true=1, false=0*/
+
+/* Number(숫자)
+const what=123;*/
+
+/* Float(floating number / 소숫점)
+const what = 12.3;*/
