816e0070f8be91f0a1045df3c2f741821df72a39
hiryu826 <45277948+hiryu826@users.noreply.github.com>
2019-12-06 00:06:32 +0900

    2019.12.06

--- Index.js
+++ Index.js
@@ -1,11 +1,43 @@
-// 20191028
-console.log('My name is Ryu Se Hyun, I can do it');
+const title = document.querySelector("#title");
 
-const Num = 100 * 100;
-let a = 'Ryu';
-let b = a + 'Sehyun'; 
-a = 100;
-console.log(Num, b, a); 
+const CLICKED_CLASS = "clicked"
+
+function handleClick(){
+    const currentClass = title.className;
+    if(currentClass !== CLICKED_CLASS){
+        title.className = CLICKED_CLASS;
+    } else {
+        title.className = "";
+    }
+}
+
+function init(){
+    title.addEventListener("mouseenter", handleClick);
+}
+
+init();
+
+// 20191028
+// console.log('My name is Ryu Se Hyun, I can do it');
+
+// const Num = 100 * 100;
+// let a = 'Ryu';
+// let b = a + 'Sehyun'; 
+// a = 100;
+// console.log(Num, b, a); 
+
+// // Array: 텍스트, true, false, numbers, floats 등만 사용 가능
+// const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+// const hyunInfo = {
+//     Name: "Sehyun",
+//     Age: 28,
+//     Gender: "Male",
+//     Married: true
+// };
+
+// console.log(daysOfWeek);
+// console.log(daysOfWeek[2]);
+// console.log(hyunInfo.Name);
 
 /* String(문자열 - Text/모든걸 같이 붙인다.)
 const what = "String"
